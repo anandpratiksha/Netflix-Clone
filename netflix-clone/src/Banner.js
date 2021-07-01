628d7a2234139b5ce9d08342eee5ecf528061c84
anandpratiksha <apratiksha1137@gmail.com>
2021-07-02 03:34:16 +0530

    clone

--- netflix-clone/src/Banner.js
+++ netflix-clone/src/Banner.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 
 function Banner() {
     const [movie, setMovie] = useState([]);
-
     return (
         <header> { /* Background image */}
             {/* Title */}
