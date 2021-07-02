a782cf775cfa1163d8dc5603cc7ca336f03b52a5
anandpratiksha <apratiksha1137@gmail.com>
2021-07-02 23:38:34 +0530

    Clone

--- netflix-clone/src/Banner.js
+++ netflix-clone/src/Banner.js
@@ -1,7 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from './axios';
+import requests from './request';
 
 function Banner() {
     const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            setMovie(
+                request.data.results[
+                Math.floor(Math.random() * request.data.results.length - 1
+                )]
+            );
+            return requests;
+        }
+        fetchData();
+    }, []);
+
     return (
         <header> { /* Background image */}
             {/* Title */}
