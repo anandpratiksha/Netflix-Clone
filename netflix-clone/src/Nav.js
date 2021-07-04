325b9e124cabf7698675d795caf5d899b7a5244f
anandpratiksha <apratiksha1137@gmail.com>
2021-07-04 23:42:44 +0530

    Clone

--- netflix-clone/src/Nav.js
+++ netflix-clone/src/Nav.js
@@ -2,14 +2,15 @@ import React from 'react';
 import logo1 from './nf.png';
 import logo2 from './logo.png';
 import './Nav.css';
+import { useState, useEffect } from 'react';
 
 function Nav() {
     const [show, handleShow] = useState(false)
 
     useEffect(() => {
         window.addEventListener('scroll', () => {
-            if (window.scroll > 100) {
-                handleShow(true)
+            if (window.scrollY > 100) {
+                handleShow(true);
             } else handleShow(false);
         });
         return () => {
@@ -19,7 +20,7 @@ function Nav() {
 
     return (
         <div>
-            <div className="nav">
+            <div className={`nav ${show && "nav__black"}`}>
                 <img className="nav__logo" src={logo1} alt="NetFlix-logo" />
                 <img className="nav__avtar" src={logo2} alt="netflix-logo" />
             </div>
